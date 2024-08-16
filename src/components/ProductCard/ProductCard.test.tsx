9eb5d3bcd8d8087caa16e57c5a4a2a2f19049efa
muammertopcu <muammertop@outlook.com>
2024-08-17 01:13:46 +0300

    edited product card actions and tests

--- src/components/ProductCard/ProductCard.test.tsx
+++ src/components/ProductCard/ProductCard.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 import ProductCard from './ProductCard.tsx';
 
 import product from '__mocks__/product.json';
@@ -18,5 +18,19 @@ describe('<ProductCard />', () => {
     expect(getByText(product.title)).toBeTruthy();
     expect(getByText(product.category)).toBeTruthy();
     expect(getByText(`$${product.price}`)).toBeTruthy();
+  });
+
+  it('should navigate to the product detail screen', () => {
+    const mockNavigate = jest.fn();
+
+    jest
+      .spyOn(require('@react-navigation/native'), 'useNavigation')
+      .mockReturnValue({navigate: mockNavigate});
+
+    const {getByTestId} = render(<ProductCard product={product} />);
+
+    fireEvent(getByTestId('product-card-container'), 'onPress');
+
+    expect(mockNavigate).toHaveBeenCalledWith('ProductDetail', {productId: product.id.toString()});
   });
 });
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -8,10 +8,16 @@ import {
   ProductName,
 } from './ProductCard.styled.ts';
 import type {ProductCardProps} from './ProductCard.types.ts';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const ProductCard = ({product}: ProductCardProps) => {
+  const navigation = useNavigation<NativeStackNavigationProp<HomeStackParamList>>();
+
   return (
-    <Container testID={'product-card-container'}>
+    <Container
+      testID={'product-card-container'}
+      onPress={() => navigation.navigate('ProductDetail', {productId: product.id.toString()})}>
       <ProductImage source={{uri: product.thumbnail}} />
 
       <ProductInfo>
